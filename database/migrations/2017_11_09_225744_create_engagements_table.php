76fde732ea09389de9fc1d5e3a7c2e10e61bda4b
Hao Xiong <haoiong@outlook.com>
2017-11-09 21:51:51 -0600

    retouch database migrations

--- database/migrations/2017_11_09_225744_create_engagements_table.php
+++ database/migrations/2017_11_09_225744_create_engagements_table.php
@@ -21,13 +21,15 @@ class CreateEngagementsTable extends Migration
             $table->date('close_date')->nullable();
             $table->unsignedInteger('leader_id');
             $table->unsignedInteger('collaboration_id');
-            $table->float('buz_dev_share')->nullable();
+            $table->float('buz_dev_share')->default(0);
             //used to indicate whether the engagement has closed or sth
-            $table->unsignedTinyInteger('status')->nullable();
-            //client paid by 1. hourly or 2. month-fixed or 3. engagement-fixed
-            $table->unsignedTinyInteger('billing_type');
-            //the total amount of money should bill the client
-            $table->decimal('billing_total',15,2)->nullable();
+            $table->unsignedTinyInteger('status')->default(0);
+            //indicate how the client pay,0=engagement fixed,1=/hour,2=/15-day,3=/month,4=/year...
+            $table->unsignedTinyInteger('paying_cycle')->default(0);
+            //billing amount to client every term/cycle
+            $table->decimal('billing_amount',15,2)->default(0);
+            $table->decimal('billing_total',15,2)->default(0);
+            $table->decimal('operating_profit',15,2)->default(0);
             $table->timestamps();
         });
     }
