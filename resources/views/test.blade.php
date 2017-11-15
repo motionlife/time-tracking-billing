ba20d7c21e9915d2adbf3ebf1f87635a909bf14e
Hao Xiong <haoiong@outlook.com>
2017-11-14 20:19:42 -0600

    All model has been tested on real data. Migration success. Prepare building up features.

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="">
+        <?php
+        $j=0;
+        ?>
         @foreach($consultants as $i=>$consul)
             <div>
                 <h3>{{$consul->fullname()}}</h3>
@@ -9,6 +12,9 @@
                     <li>Total Billable Hours:{{$result[$i]['totalbh']}}</li>
                     <li>Total Nob-billable Hours:{{$result[$i]['totalnbh']}}</li>
                     <li>Total Paid:<strong>${{number_format($result[$i]['totalpay'],2)}}</strong></li>
+                    @if($result[$i]['totalpay'])
+                        <li>Reference:<strong>${{number_format($csv[$j++],2)}}</strong></li>
+                    @endif
                 </ur>
             </div>
             <hr>
