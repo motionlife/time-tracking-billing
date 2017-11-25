44de7e7a8383d86fcf0d2d514a5429b7b149f2a4
Hao Xiong <haoiong@outlook.com>
2017-11-25 07:09:17 -0600

    engagement page client filter optimization

--- resources/views/auth/pending.blade.php
+++ resources/views/auth/pending.blade.php
@@ -8,8 +8,8 @@
             <div class="vertical-align-middle">
                 <div class="auth-box lockscreen clearfix" style=" margin-top: 7em">
                     <div class="content" style="background-color: #fff; color: #636b6f; font-family: 'Raleway', sans-serif;font-weight: 100;">
-                        <img src="/img/processing-s.gif" alt="">
-                        
+                           <img src="/img/processing-s.gif" alt='<i class="fa fa-hourglass-start" aria-hidden="true"></i>' style="margin-left: 10%;">
+
                         <h2>Hi! {{Auth::user()->first_name}}, we will send you an email as soon as the admin verifies
                             your application to use this system. </h2>
 
