62ba4d691160d51342f42aa6aa248bf120fb0a16
Hao Xiong <haoiong@outlook.com>
2018-01-01 03:15:09 -0600

    Code refactoring

--- app/Models/Engagement.php
+++ app/Models/Engagement.php
@@ -115,19 +115,10 @@ class Engagement extends Model
 
     public function HourBilling($start = null, $end = null, $review_state = null)
     {
-        //For monthly labor billing, detail not implemented yet...
-        //todo: dealing with different client-billing type should be scrutinized when do the billing
         if ($this->paying_cycle == 0) {
-            $total = 0;
-            //in case some consultant was removed from the engagement
-            foreach ($this->arrangements()->withTrashed()->get() as $arr) {
-                $hours = $arr->hours()->whereBetween('report_date', [$start ?: '1970-01-01', $end ?: '2038-01-19'])
-                    ->where('review_state', isset($review_state) ? '=' : '<>', isset($review_state) ? $review_state : 7)->get();
-                foreach ($hours as $hour) {
-                    $total += $hour->billClient();
-                }
-            }
-            return $total;
+            return Hour::reported($start,$end,[$this->id],null,$review_state,null)->sum(function ($hour){
+                return $hour->billClient();
+            });
         } else {
             return 0;
         }
@@ -184,7 +175,7 @@ class Engagement extends Model
     {
         $aids = collect();
         foreach ($eids as $eid) {
-            $aids->push(self::find($eid)->arrangements->pluck('id'));
+            $aids->push(self::find($eid)->arrangements()->withTrashed()->get()->pluck('id'));
         }
         return $aids->flatten();
     }
